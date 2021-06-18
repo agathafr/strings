78c93994300c4e35950917a40b50476def4fcc07
Agatha França <agathafranca@outlook.com>
2021-06-18 18:08:41 -0300

    Adicionar arquivos de projeto.

---
+++ StudentBank.AgencySystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentBank.Models;
+using StudentBank.Models.Funcionarios;
+using Humanizer;
+
+namespace StudentBank.AgencySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(7);
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+
+        static void diferencaDatas()
+        {
+            DateTime dataFimPagamento = new DateTime(2021, 6, 30);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40); //dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            //Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ StudentBank.Models/AutenticacaoHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentBank.Models
+{
+    class AutenticacaoHelper
+    {
+        public bool CompararSenhas(string senhaVerdadeira, string senhaTentativa)
+        {
+            return senhaVerdadeira == senhaTentativa;
+        }
+    }
+}
